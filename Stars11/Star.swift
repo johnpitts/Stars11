a6e9da617b4c2b6636e4ab11b5089b43bbce6821
John Pitts <jgpitts@gmail.com>
2019-10-15 12:51:07 -0400

    Added Star model and StarController files.  distance not quite sure which type to use yet.

---
+++ Stars11/Star.swift
@@ -0,0 +1,17 @@
+//
+//  Star.swift
+//  Stars11
+//
+//  Created by John Pitts on 10/15/19.
+//  Copyright © 2019 johnpitts. All rights reserved.
+//
+
+import Foundation
+
+
+struct Star {
+    
+    let name: String
+    let distance: Double
+    
+}
---
+++ Stars11/StarController.swift
@@ -0,0 +1,19 @@
+//
+//  StarController.swift
+//  Stars11
+//
+//  Created by John Pitts on 10/15/19.
+//  Copyright © 2019 johnpitts. All rights reserved.
+//
+
+import Foundation
+
+// the viewcontroller's job is simply to display a view, not manipulate data/model, so this is why we have a modelController (starController) to do all the heavy lifting with the data
+class StarController {
+    
+    var stars: [Star] = []
+    
+    
+    
+    
+}
